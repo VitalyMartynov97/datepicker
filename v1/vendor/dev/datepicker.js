6f2d79495b198fc6f5dfe3c76d624194f83e8596
Vitaly Martynov <doroadsy@gmail.com>
2023-11-29 23:55:28 +0300

    fix-lib-start

--- v1/vendor/dev/datepicker.js
+++ v1/vendor/dev/datepicker.js
@@ -14,45 +14,58 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 });
 
-function whenAvailable(name, callback) {
-    var interval = 1; // ms
+function whenAvailable(names, callback) {
+    var interval = 100; 
+
     window.setTimeout(function() {
-        if (window[name]) {
-            callback(window[name]);
+        let all_upload = false
+        for (lib_index in names)
+        {
+            all_upload = window[names[lib_index]]
+        }
+        if (all_upload) {
+            callback(window[names[0]]);
         } else {
-            whenAvailable(name, callback);
+            whenAvailable(names, callback);
         }
     }, interval);
 }
 
-whenAvailable("jQuery", function(t) {
-    addDaemons();
-});
+function getContainers() {
+    return $('.'+CLASS_NAME);
+}
 
-// document.addEventListener('DOMContentLoaded', function(){
-//     let urls = [
-//         'https://code.jquery.com/jquery-3.6.3.min.js',
-//         'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js'
-//     ]; 
-
-//     for (url_index in urls)
-//     {
-//         let script = document.createElement('script')
-//         script.src = urls[url_index]
-//         document.getElementsByTagName('head')[0].appendChild(script)
-//     }
-
-//     addDaemons()
-// });
-
-function addDaemons() {
-    let input_list = $('.'+CLASS_NAME)
+function addNowDates() {
+    let input_list = getContainers()
 
     for (input_index in input_list)
     {
-        input_list[input_index].value = '123'
+        input_list[input_index].value = moment().format('YYYY-MM-DD')
     }
 }
 
+function addDaemons() {
+    let input_list = getContainers()
 
+    for (input_index in input_list)
+    {
+       // input_list[input_index].value = '123'
+    }
+}
 
+function setStyle() {
+    let input_list = getContainers()
+
+    for (input_index in input_list)
+    {
+       input_list[input_index].style = 'cursor: pointer;'
+    }
+}
+
+// code starts...
+
+whenAvailable(['jQuery', 'moment'], function(t) {
+    setStyle()
+    addNowDates()
+    addDaemons()
+});
